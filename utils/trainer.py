e3786e1187b6406bdd6c35a49dd2b9d2f5cf004c
KevinChen880723 <B10602120@mail.ntust.edu.tw>
2022-04-26 21:23:04 +0800

    Add keep train

--- utils/trainer.py
+++ utils/trainer.py
@@ -25,9 +25,13 @@ class Trainer:
 
     def train(self):
         total_iterations = self.cfg['train']['total_iterations']
-        progress_bar = tqdm(range(total_iterations), total=total_iterations, desc='Total iteration: ')
         train_iter = iter(self.dataloader_train)
         self.model = self.model.to(self.device)
+        iteration_start = 0
+        if self.cfg['keep_train']:
+            iteration_start = self.load_model()+1
+        progress_bar = tqdm(range(iteration_start, total_iterations+iteration_start), desc='Total iteration: ', initial=iteration_start)
+
         for iteration in progress_bar:
             try:
                 data = next(train_iter)
@@ -53,6 +57,7 @@ class Trainer:
 
             if (iteration+1) % self.cfg['train']['eval_freq'] == 0:
                 self.val(iteration+1)
+            if (iteration+1) % self.cfg['train']['save_model_freq'] == 0:
                 self.save_model(iteration+1)
 
     def val(self, iteration):
@@ -95,3 +100,13 @@ class Trainer:
             'iteration': iteration
         }
         torch.save(checkpoint, "{}/{}/iteration_{}.pth".format(self.cfg['output']['output_folder'], self.cfg['output']['description'], iteration))
+
+    def load_model(self):
+        checkpoint = torch.load(self.cfg['path_pretrained_weight'])
+        if 'model' in self.cfg['keep_train_obj']:
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+        if 'optimizer' in self.cfg['keep_train_obj']:
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        if 'lr_scheduler' in self.cfg['keep_train_obj']:
+            self.lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        return checkpoint['iteration']
